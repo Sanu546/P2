e5be6b7c48e815dbd44fc10f8272a45186daac40
Andreas Overgaard Barbre <ao.barbre@gmail.com>
2025-03-17 15:07:20 +0100

    Basic controlls done

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import sys
 from rtde_stuff.rtde_com import RTDEConnection
 import time
+import numpy as np
 
 UR5 = RTDEConnection() # Connnect to the UR5 robot
 
 def main():
-    time.sleep(6)
-    UR5.moveToTCP([0.29, -0.25, 0.50, 0.02, 2.8, -1.362]) # Move to a specific position
-    print(f"Moved to position({str(UR5.getCurrentPos())})")
-    time.sleep(2)
-    UR5.moveToTCP([0.43, -0.35, 0.50, 0.50, 1.5, -1.362]) # Move to a specific position
+    #Example code
+    print("Ready to execute commands")
+    time.sleep(4)
+    UR5.moveJoint([np.radians(-41.62), np.radians(-66.38), np.radians(-110.76), np.radians(56.17), np.radians(93.86), np.radians(38.53)]) # Move to a specific position
+    UR5.moveTCP([0.29, -0.25, 0.50, 0.02, 2.8, -1.362]) # Move to a specific position
+    print("Move added to queue")
+    UR5.moveTCPandWait([0.40, -0.55, 0.20, 0.50, 1.5, -1.362], "l") # Move to a specific position
     print(f"Moved to final position({str(UR5.getCurrentPos())})")
+    UR5.setToolPos(69)
     time.sleep(2)
-    UR5.kill()
+    
+    
+    #Stuff so you can ctrl+c to exit the program
+    while True:
+        pass
+    #UR5.kill()# Kill the connection to the robot, so the server thread stops running
 
-
+#stuff so you can ctrl+c to exit the program
 try:
     main()
     
--- rtde_stuff/rtde_com.py
+++ rtde_stuff/rtde_com.py
@@ -9,14 +9,14 @@ logging.getLogger().setLevel(logging.INFO)
 import rtde.rtde as rtde
 import rtde.rtde_config as rtde_config
 import threading as th #Import treading to keep the server running in the background
+import numpy as np
 
 
 class RTDEConnection:
     config_filename = "rtde_stuff/control_loop_configuration.xml"
-    com_active = False 
     killed = False   
     #constructor
-    def __init__(self, ip_address='192.168.56.101', port=30004, startPos=[-0.12, -0.43, 0.14, 0, 3.11, 0.04]):
+    def __init__(self, ip_address='192.168.56.101', port=30004, startPos=[np.radians(-91.772925), np.radians(-99.491749), np.radians(-126.807916), np.radians(-45.229147), np.radians(91.748872), np.radians(-1.796248)]):
         
         #Establish a connection to the UR robot
         self.con = rtde.RTDE(ip_address, port)
@@ -24,16 +24,21 @@ class RTDEConnection:
         print("Connected to robot")
 
         self.con.get_controller_version()
+        
+        self.targets = []
+        self.moving = False
 
         #Make recipes from configuration file(The recipes are the data that will be sent to and from the robot)
         self.conf = rtde_config.ConfigFile(self.config_filename)
         self.state_names, self.state_types = self.conf.get_recipe("state")
         self.setPos_names, self.setPos_types = self.conf.get_recipe("setPose")
+        self.setTool_names, self.setTool_types = self.conf.get_recipe("setTool")
         self.watchdog_names, self.watchdog_types = self.conf.get_recipe("watchdog")
 
         #Setup the recipes on the robot
         self.con.send_output_setup(self.state_names, self.state_types)
         self.setPos = self.con.send_input_setup(self.setPos_names, self.setPos_types)
+        self.setTool = self.con.send_input_setup(self.setTool_names, self.setTool_types)
         self.watchdog = self.con.send_input_setup(self.watchdog_names, self.watchdog_types)
 
         #Initialize the setPose
@@ -44,7 +49,11 @@ class RTDEConnection:
         self.setPos.input_double_register_4 = 0
         self.setPos.input_double_register_5 = 0
         self.setPos.input_int_register_1 = 0
+        self.setPos.input_int_register_2 = 0
 
+        #Initialize the setTool
+        self.setTool.input_int_register_3 = 0
+        
         #Initialize the watchdog
         self.watchdog.input_int_register_0 = 0
 
@@ -55,59 +64,85 @@ class RTDEConnection:
         program_running = False
 
         print("Waiting for robot to start program")
-
+        
+        #Start a thread to keep the server running
+        self.server_thread = th.Thread(target=self.serverThread)
+        self.server_thread.start()
+        
         while not program_running:
             state = self.con.receive()
             if state.output_int_register_0 == 1:
                 program_running = True
          
-        self.moveToTCP(startPos)
+        self.moveJointandWait(startPos)
         print("Robot is in position and ready to receive commands")
+    
+    
+    def moveTCP(self, position, type="j"):
+        self.targets.append({"position": position, "joint": False, "type": type})
 
-        #Start a thread to keep the server running
-        self.server_thread = th.Thread(target=self.keep_server_running)
-        self.server_thread.start()
-
-    def moveToTCP(self, jointArray):
-        self.com_active = True
-
-        move_completed = False
-        #Save the joint positions to input registers
-        list_to_setPos(self.setPos, jointArray)
-
-        #Set the move type to joints
-        self.setPos.input_int_register_1 = 0
-
-        #Send the move to the robot
-        self.con.send(self.setPos)
-        self.watchdog.input_int_register_0 = 1
-        while not move_completed:
-            #Receive the current state
-            state = self.con.receive()
-
-            #Check if the move is completed
-            if state.output_int_register_0 == 0:
-                move_completed = True
-                self.watchdog.input_int_register_0 = 0
-
-            self.con.send(self.watchdog)
-
-        self.com_active = False
+    
+    def moveTCPandWait(self, position, type="j"):
+        self.targets.append({"position": position, "joint": False, "type": type})
+        while len(self.targets) > 0:
+            pass
+     
+    def moveJoint(self, position):
+        self.targets.append({"position": position, "joint": True, "type": "j"})
+           
+    def moveJointandWait(self, position):
+        self.targets.append({"position": position, "joint": True, "type": "j"})
+        while len(self.targets) > 0:
+            pass
 
     #Get the current position of the robot
     def getCurrentPos(self):
-        self.com_active = True
         state = self.con.receive()
-        self.com_active = False
         return state.actual_TCP_pose
     
-    #Keep the server running in the background
-    def keep_server_running(self):
-        while not self.killed:
-            if not self.com_active:
-                    self.con.receive()
-                    self.con.send(self.watchdog)
+    def getCurrentTaget(self):
+        state = self.con.receive()
+        return state.target_TCP_pose
     
+    def getAllTargets(self):
+        return self.targets
+    
+    def setToolPos(self, tool):
+        self.setTool.input_int_register_3 = tool
+    
+    #Keep the server running in the background
+    def serverThread(self):
+        while not self.killed:
+            if len(self.targets) > 0: #If there are targets in the queue
+                print("Moving to target")
+                target = self.targets[0]
+                print(target)
+                moveDone = False
+                
+                self.setPos.input_int_register_1 = 0 if target["joint"] else 1
+                self.setPos.input_int_register_2 = 0 if target["type"] == "j" else 1
+                 
+                list_to_setPos(self.setPos, target["position"])
+                self.con.send(self.setPos)
+                self.watchdog.input_int_register_0 = 1
+                state = self.con.receive()
+                    
+                while not moveDone and state.output_int_register_0 == 1:
+                    state = self.con.receive()
+                    #print(f"Robot out: {state.output_int_register_0}")
+                    if state.output_int_register_0 == 0:
+                        print("Move done")
+                        moveDone = True
+                        self.watchdog.input_int_register_0 = 0
+                    self.con.send(self.watchdog)
+                    
+                self.targets.pop(0)
+            else:
+                state = self.con.receive()
+                #print(f"Robot out: {state.output_int_register_0}")
+                self.con.send(self.setTool)
+                self.con.send(self.watchdog)
+        
     #Kill the connection to the robot and stop the server thread
     def kill(self):
         self.killed = True
@@ -117,14 +152,11 @@ class RTDEConnection:
         print("Disconnected from robot")
 
 
-
- 
 def setPos_to_list(sp):
     sp_list = []
     for i in range(0, 6):
         sp_list.append(sp.__dict__["input_double_register_%i" % i])
     return sp_list
-
 
 def list_to_setPos(sp, list):
     for i in range(0, 6):
