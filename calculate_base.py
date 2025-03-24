ff2b13e0d931abe9ee07ed8e62e36ecbae089d91
Benjamin Hermansen <bshermansen@gmail.com>
2025-03-24 11:19:51 +0100

    Added support for spatial math SE3. And finalised and tested.

--- calculate_base.py
+++ calculate_base.py
@@ -1,20 +1,21 @@
 import numpy as np
 from numpy import linalg
-from numpy import pi
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import spatialmath as sm
 
-points = [[22.8,  15.3, 0],
-          [126.7, 51.5, 0],
-          [-1.5,  96.7, 30]]
+points = np.array([ [12.8, -5.3, 7],
+                    [6.7, 1.5, 6],
+                    [-7.5, -9.7, 3]])
 
 def calculate_base(points):
-    
-    # Convert to numpy array
-    points = np.array(points)
-    
+    """Takes 3 points from a np array as\n
+    [[x1,y1,z1], [x2,y2,z2], [x3,y3,z3]]\n
+    and returns the corrospodning transformation matrix where [x1,y1,z1] is the origin.\n
+    [x2,y2,z2] represents the direction of the X-axis, and [x3,y3,z3] represents the direction of the Y-axis.\n
+    Returns: SE3 spatial math transform and np.array 4x4 transform
+    """
     # Print the input
-    print(f"\nInput points\n{points}")
+    print(f"\nInput points to calculate base\n{points}")
     
     # Calculate and normalize x vector
     x = points[1] - points[0]
@@ -23,40 +24,37 @@ def calculate_base(points):
     # Norm x, using ord=2 assures that the direction of x is maintained when normalizing
     x_norm = x * 1 / linalg.norm(x, ord=2)
     
-    # Project vector from origo to y onto vector x to make it ortogonal with x
-    # Calculate and normalize y vector
+    # Calculate y vector
     y = points[2] - points[0]
     assert linalg.norm(y) != 0, "\n\nOrigo and y is the same point!!!\n"
     
     # Calculate projection of y onto x
     y_orth = y - (np.dot(y,x) / np.dot(x,x)) * x
-    #linalg.norm(x, ord=2)
+    
     # Norm y, using ord=2 assures that the direction of y is maintained when normalizing
     y_norm = y_orth * 1 / linalg.norm(y_orth, ord=2)
 
-    
     #Calculate the last vector z which is orthogonal on x and y and therefore can be found by their cross product
     z_norm = np.cross(x_norm,y_norm)
     
-    # Final assersions
-    assert np.isclose(linalg.norm(x_norm, ord=2), 1, 0.00001) or np.isclose(linalg.norm(x_norm, ord=2), -1, 0.00001), f"\n\nLength of x_norm is {linalg.norm(x_norm, ord=2)} which is not 1 or -1\n"
-    assert np.isclose(linalg.norm(y_norm, ord=2), 1, 0.00001) or np.isclose(linalg.norm(y_norm, ord=2), -1, 0.00001), f"\n\nLength of y_norm is {linalg.norm(y_norm, ord=2)} which is not 1 or -1\n"
-    assert np.isclose(linalg.norm(z_norm, ord=2), 1, 0.00001) or np.isclose(linalg.norm(z_norm, ord=2), -1, 0.00001), f"\n\nLength of z_norm is {linalg.norm(z_norm, ord=2)} which is not 1 or -1\n"
+    # Assersions
+    assert np.isclose(linalg.norm(x_norm, ord=2), 1, 0.000001) or np.isclose(linalg.norm(x_norm, ord=2), -1, 0.000001), f"\n\nLength of x_norm is {linalg.norm(x_norm, ord=2)} which is not 1 or -1\n"
+    assert np.isclose(linalg.norm(y_norm, ord=2), 1, 0.000001) or np.isclose(linalg.norm(y_norm, ord=2), -1, 0.000001), f"\n\nLength of y_norm is {linalg.norm(y_norm, ord=2)} which is not 1 or -1\n"
+    assert np.isclose(linalg.norm(z_norm, ord=2), 1, 0.000001) or np.isclose(linalg.norm(z_norm, ord=2), -1, 0.000001), f"\n\nLength of z_norm is {linalg.norm(z_norm, ord=2)} which is not 1 or -1\n"
     
-    assert np.isclose(np.dot(x_norm,y_norm), 0, 0.00001), f"\n\nx_norm and y_norm are not orthogonal their dot product is {np.dot(x_norm,y_norm)} which is not 0\n"
-    assert np.isclose(np.dot(x_norm,z_norm), 0, 0.00001), f"\n\nx_norm and z_norm are not orthogonal their dot product is {np.dot(x_norm,z_norm)} which is not 0\n"
+    assert np.isclose(np.dot(x_norm,y_norm), 0, 0.000001), f"\n\nx_norm and y_norm are not orthogonal their dot product is {np.dot(x_norm,y_norm)} which is not 0\n"
+    assert np.isclose(np.dot(x_norm,z_norm), 0, 0.000001), f"\n\nx_norm and z_norm are not orthogonal their dot product is {np.dot(x_norm,z_norm)} which is not 0\n"
 
     # Build transform matrix from points
     base = np.array([x_norm, y_norm, z_norm, points[0]]).T
     base = np.vstack([base, [0,0,0,1]])
-        
-    print(f"\nResulting transform:\n{base}")
     
-    plot_points_and_transforms(points, [base,  np.array([[1,0,0,1],
-                                                        [0,1,0,1],
-                                                        [0,0,1,1],
-                                                        [0,0,0,1]])])
-
+    # Convert tranform into spatialmath transform
+    transform = sm.SE3.Rt(base[0:3, 0:3], base[0:3, 3])
+    
+    print(f"\nResulting transform:\n{transform}")
+    
+    return transform, base
 
 def plot_points_and_transforms(points, transforms):
     # Create a 3D plot
@@ -104,6 +102,8 @@ def plot_points_and_transforms(points, transforms):
     
     # Show the plot
     plt.show()
+    
+_, base = calculate_base(points)
 
-
-calculate_base(points)
+# Debugging
+plot_points_and_transforms(points, [base,  np.array([[1,0,0,1], [0,1,0,1], [0,0,1,1], [0,0,0,1]])])
