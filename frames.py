f73993a1f2df67f9fb752122d981266978099c1e
Andreas Overgaard Barbre <ao.barbre@gmail.com>
2025-03-26 14:59:44 +0100

    init

--- frames.py
+++ frames.py
@@ -2,7 +2,8 @@ import numpy as np
 import matrixConversion as mc
 
 class Pose:
-    def __init__(self, matrix, base=None, approach=0.05, isCell=False, color=None):
+    def __init__(self, name, matrix, base=None, approach=0.05, isCell=False, color=None):
+        self.name = name
         self.isCell = isCell
         self.matrix = matrix
         self.base = base
