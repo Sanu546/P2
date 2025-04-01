18d4231154f6fea8e1d8d84c1770bfed6c7253a6
Andreas Overgaard Barbre <ao.barbre@gmail.com>
2025-04-01 11:06:56 +0200

    Position display done

--- gui/P2_GUI.py
+++ gui/P2_GUI.py
@@ -19,7 +19,7 @@ class CellDisplay(QWidget):
         self.initUI()
 
     def initUI(self):
-        # Set up the layout
+        # Set up the layout 
         self.grid_layout = QGridLayout()
         #self.input_layout = QVBoxLayout()
 
@@ -240,6 +240,8 @@ class Calibrator(QWidget):
     leftFunc = None
     rightFunc = None
     stopActionFunc = None
+    upUpFunc = None
+    downDownFunc = None
     
     def __init__(self):
         super().__init__()
@@ -248,6 +250,82 @@ class Calibrator(QWidget):
     def initUI(self):
         # Create a label to display the pressed arrow key
         self.startCal = QPushButton("Start Calibration")
+        self.saveCal = QPushButton("Save Calibration")
+        self.resetCal = QPushButton("Reset Calibration")
+        self.stopCal = QPushButton("Stop Calibration")
+        
+        self.startCal.setFont(QFont("Arial", 10, QFont.Weight.Bold))
+        self.saveCal.setFont(QFont("Arial", 10, QFont.Weight.Bold))
+        self.resetCal.setFont(QFont("Arial", 10, QFont.Weight.Bold))
+        self.stopCal.setFont(QFont("Arial", 10, QFont.Weight.Bold))
+        
+        self.startCal.setFixedHeight(30)
+        self.saveCal.setFixedHeight(30)
+        self.resetCal.setFixedHeight(30)
+        self.stopCal.setFixedHeight(30)
+        
+        self.spacing = QLabel("")
+        self.spacing.setFixedHeight(10)
+        
+        self.spacing2 = QLabel("")
+        self.spacing2.setFixedHeight(10)
+        
+        self.xTitle = QLabel("X:")
+        self.yTitle = QLabel("Y:")
+        self.zTitle = QLabel("Z:")
+        self.rotXTitle = QLabel("Rot X:")
+        self.rotYTitle = QLabel("Rot Y:")
+        self.rotZTitle = QLabel("Rot Z:")
+        
+        self.xTitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.yTitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.zTitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.rotXTitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.rotYTitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.rotZTitle.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        self.xTitle.setFont(QFont("Arial", 12, QFont.Weight.Bold))
+        self.yTitle.setFont(QFont("Arial", 12, QFont.Weight.Bold))
+        self.zTitle.setFont(QFont("Arial", 12, QFont.Weight.Bold))
+        self.rotXTitle.setFont(QFont("Arial", 12, QFont.Weight.Bold))
+        self.rotYTitle.setFont(QFont("Arial", 12, QFont.Weight.Bold))
+        self.rotZTitle.setFont(QFont("Arial", 12, QFont.Weight.Bold))
+        
+        self.xValue = QLineEdit()
+        self.yValue = QLineEdit()
+        self.zValue = QLineEdit()
+        self.rotXValue = QLineEdit()
+        self.rotYValue = QLineEdit()
+        self.rotZValue = QLineEdit()
+        
+        self.xValue.setFont(QFont("Arial", 12, QFont.Weight.Bold))
+        self.yValue.setFont(QFont("Arial", 12, QFont.Weight.Bold))
+        self.zValue.setFont(QFont("Arial", 12, QFont.Weight.Bold))
+        self.rotXValue.setFont(QFont("Arial", 12, QFont.Weight.Bold))
+        self.rotYValue.setFont(QFont("Arial", 12, QFont.Weight.Bold))
+        self.rotZValue.setFont(QFont("Arial", 12, QFont.Weight.Bold))
+        
+        self.xValue.setFixedHeight(30)
+        self.yValue.setFixedHeight(30)
+        self.zValue.setFixedHeight(30)
+        self.rotXValue.setFixedHeight(30)
+        self.rotYValue.setFixedHeight(30)
+        self.rotZValue.setFixedHeight(30)
+        
+        self.xValue.setFixedWidth(100)
+        self.yValue.setFixedWidth(100)
+        self.zValue.setFixedWidth(100)
+        self.rotXValue.setFixedWidth(100)   
+        self.rotYValue.setFixedWidth(100)
+        self.rotZValue.setFixedWidth(100)
+        
+        self.xValue.setReadOnly(True)
+        self.yValue.setReadOnly(True)
+        self.zValue.setReadOnly(True)
+        self.rotXValue.setReadOnly(True)
+        self.rotYValue.setReadOnly(True)
+        self.rotZValue.setReadOnly(True)
+        
         self.label = QLabel("Calibrating Buttons:")
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.labelLeft = QLabel("Rotate Left:")
@@ -257,8 +335,40 @@ class Calibrator(QWidget):
         self.buttonDown = QPushButton("↓")
         self.buttonLeft = QPushButton("←")
         self.buttonRight = QPushButton("→")
+        self.buttonUpUp = QPushButton("↑")
+        self.buttonDownDown = QPushButton("↓")
+        
         self.buttonRotLeft = QPushButton("←")
         self.buttonRotRight = QPushButton("→")
+        
+        self.buttonDownDown.setFixedWidth(23)
+        self.buttonUpUp.setFixedWidth(23)
+        self.buttonUpUp.setFixedHeight(58)
+        self.buttonDownDown.setFixedHeight(58)
+        
+        self.buttonUp.setEnabled(False)
+        self.buttonDown.setEnabled(False)
+        self.buttonLeft.setEnabled(False)
+        self.buttonRight.setEnabled(False)
+        self.buttonRotLeft.setEnabled(False)
+        self.buttonRotRight.setEnabled(False)
+        self.startCal.setEnabled(True)
+        self.stopCal.setEnabled(False)
+        self.saveCal.setEnabled(False)
+        self.resetCal.setEnabled(False)
+        self.buttonUpUp.setEnabled(False)
+        self.buttonDownDown.setEnabled(False)
+        
+        self.startCal.clicked.connect(self.startCalibration)
+        self.stopCal.clicked.connect(self.stopCalibration)
+        self.resetCal.clicked.connect(self.resetCalibration)
+        
+        self.buttonUp.released.connect(self.adjustmentStop)
+        self.buttonDown.released.connect(self.adjustmentStop)
+        self.buttonLeft.released.connect(self.adjustmentStop)
+        self.buttonRight.released.connect(self.adjustmentStop)
+        self.buttonRotLeft.released.connect(self.adjustmentStop)
+        self.buttonRotRight.released.connect(self.adjustmentStop)
 
         self.dropdown = QComboBox()
         self.dropdown.currentIndexChanged.connect(self.base)
@@ -277,21 +387,104 @@ class Calibrator(QWidget):
         hbox.addWidget(self.buttonLeft)
         hbox.addWidget(self.buttonDown)
         hbox.addWidget(self.buttonRight)
-
+        
+        xyTranslationBox = QVBoxLayout()
+        
+        xyTranslationBox.addWidget(self.buttonUp)
+        xyTranslationBox.addLayout(hbox)
+        
+        translationBox = QHBoxLayout()
+        translationBox.addWidget(self.buttonUpUp)
+        translationBox.addLayout(xyTranslationBox)  
+        translationBox.addWidget(self.buttonDownDown)
+        
+        
+        startSaveBox = QHBoxLayout()
+        startSaveBox.addWidget(self.startCal)
+        startSaveBox.addWidget(self.saveCal)
+        
+        stopResetBox = QHBoxLayout()
+        stopResetBox.addWidget(self.stopCal)
+        stopResetBox.addWidget(self.resetCal)
+        
+        xTitleBox = QHBoxLayout()
+        xTitleBox.addWidget(self.xTitle)
+        xTitleBox.addWidget(self.rotXTitle)
+        
+        xValueBox = QHBoxLayout()
+        xValueBox.addWidget(self.xValue)
+        xValueBox.addWidget(self.rotXValue)
+        
+        yTitleBox = QHBoxLayout()
+        yTitleBox.addWidget(self.yTitle)
+        yTitleBox.addWidget(self.rotYTitle)
+        
+        yValueBox = QHBoxLayout()
+        yValueBox.addWidget(self.yValue)
+        yValueBox.addWidget(self.rotYValue)
+        
+        zTitleBox = QHBoxLayout()
+        zTitleBox.addWidget(self.zTitle)
+        zTitleBox.addWidget(self.rotZTitle)
+        
+        zValueBox = QHBoxLayout()
+        zValueBox.addWidget(self.zValue)
+        zValueBox.addWidget(self.rotZValue)
+        
+        cooradinateBoxL = QVBoxLayout()
+        cooradinateBoxL.addWidget(self.xTitle)
+        cooradinateBoxL.addWidget(self.xValue)
+        cooradinateBoxL.addWidget(self.yTitle)
+        cooradinateBoxL.addWidget(self.yValue)
+        cooradinateBoxL.addWidget(self.zTitle)
+        cooradinateBoxL.addWidget(self.zValue)
+        
+        cooradinateBoxR = QVBoxLayout()
+        cooradinateBoxR.addWidget(self.rotXTitle)
+        cooradinateBoxR.addWidget(self.rotXValue)
+        cooradinateBoxR.addWidget(self.rotYTitle)
+        cooradinateBoxR.addWidget(self.rotYValue)
+        cooradinateBoxR.addWidget(self.rotZTitle)
+        cooradinateBoxR.addWidget(self.rotZValue)
+        
+        cooradinateBoxL.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        cooradinateBoxR.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        cooradinateBox = QHBoxLayout()
+        cooradinateBox.addLayout(cooradinateBoxL)
+        cooradinateBox.addLayout(cooradinateBoxR)
+        
+        cooradinateBox.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
         # Add widgets and layouts to the main vertical layout in the desired order
         vbox.addWidget(self.dropdown) 
-        vbox.addWidget(self.label)     
-        vbox.addLayout(rot)             
-        vbox.addWidget(self.buttonUp) 
-        vbox.addLayout(hbox)
-
+        vbox.addWidget(self.spacing)
+        vbox.addLayout(startSaveBox)
+        vbox.addLayout(stopResetBox)
+        vbox.addWidget(self.spacing2)
+        vbox.addLayout(cooradinateBox)
+        vbox.addLayout(rot)          
+        vbox.addLayout(translationBox)
+        
         self.setLayout(vbox)
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.performAction)
 
         self.currentAction = None    
-
+        
+    def setStartCalibration(self, function):
+        self.startCal.clicked.connect(function)
+    
+    def setSaveCalibration(self, function):
+        self.saveCal.clicked.connect(function)
+    
+    def setResetCalibration(self, function):
+        self.resetCal.clicked.connect(function)
+    
+    def setStopCalibration(self, function):
+        self.stopCal.clicked.connect(function)
+    
     def setFunctionUp(self,function):
         self.upFunc = function 
         self.currentAction = "up"
@@ -311,7 +504,17 @@ class Calibrator(QWidget):
         self.rightFunc = function
         self.currentAction = "right"
         self.timer.start(100)        
-
+        
+    def setFunctionUpUp(self,function):
+        self.upUpFunc = function 
+        self.currentAction = "upup"
+        self.timer.start(100)
+    
+    def setFunctionDownDown(self,function):
+        self.downDownFunc = function
+        self.currentAction = "downdown"
+        self.timer.start(100)
+        
     def setFunctionRotLeft(self,function):
         self.rotateLeftFunc = function
         self.currentAction = "rotate left"
@@ -325,15 +528,81 @@ class Calibrator(QWidget):
     def setFunctionChangeBase(self,function):
         self.dropdown.currentIndexChanged.connect(function)
         
-    def setFunctionStopAction(self,function):
-        self.buttonUp.released.connect(function)
-        self.buttonDown.released.connect(function)
-        self.buttonLeft.released.connect(function)
-        self.buttonRight.released.connect(function)
-        self.buttonRotLeft.released.connect(function)
-        self.buttonRotRight.released.connect(function)
+    def setCurrentPose(self, pose):
+        self.rotXValue.setText(f"{pose[0]:.2f}")
+        self.rotYValue.setText(f"{pose[1]:.2f}")
+        self.rotZValue.setText(f"{pose[2]:.2f}")
+        self.xValue.setText(f"{pose[3]*100:.2f}")
+        self.yValue.setText(f"{pose[4]*100:.2f}")
+        self.zValue.setText(f"{pose[5]*100:.2f}")
+        
+    def adjustmentStop(self):
         self.currentAction = None
         self.timer.stop()
+
+    def startCalibration(self):
+        print("Calibration started")
+        self.buttonUp.setEnabled(True)
+        self.buttonDown.setEnabled(True)
+        self.buttonLeft.setEnabled(True)
+        self.buttonRight.setEnabled(True)
+        self.buttonRotLeft.setEnabled(True)
+        self.buttonRotRight.setEnabled(True)
+        self.buttonUpUp.setEnabled(True)
+        self.buttonDownDown.setEnabled(True)
+        self.startCal.setEnabled(False)
+        self.stopCal.setEnabled(True)
+        self.saveCal.setEnabled(True)
+        self.resetCal.setEnabled(True)
+        self.xValue.setReadOnly(False)
+        self.yValue.setReadOnly(False)
+        self.zValue.setReadOnly(False)
+        self.rotXValue.setReadOnly(False)
+        self.rotYValue.setReadOnly(False)
+        self.rotZValue.setReadOnly(False)
+    
+    def stopCalibration(self):
+        print("Calibration stopped")
+        self.buttonUp.setEnabled(False)
+        self.buttonDown.setEnabled(False)
+        self.buttonLeft.setEnabled(False)
+        self.buttonRight.setEnabled(False)
+        self.buttonRotLeft.setEnabled(False)
+        self.buttonRotRight.setEnabled(False)
+        self.buttonUpUp.setEnabled(False)
+        self.buttonDownDown.setEnabled(False)
+        self.startCal.setEnabled(True)
+        self.stopCal.setEnabled(False)
+        self.saveCal.setEnabled(False)
+        self.resetCal.setEnabled(False)
+        self.xValue.setReadOnly(True)
+        self.yValue.setReadOnly(True)
+        self.zValue.setReadOnly(True)
+        self.rotXValue.setReadOnly(True)
+        self.rotYValue.setReadOnly(True)
+        self.rotZValue.setReadOnly(True)    
+    
+    def resetCalibration(self):
+        print("Calibration reset")
+        self.buttonUp.setEnabled(False)
+        self.buttonDown.setEnabled(False)
+        self.buttonLeft.setEnabled(False)
+        self.buttonRight.setEnabled(False)
+        self.buttonRotLeft.setEnabled(False)
+        self.buttonRotRight.setEnabled(False)
+        self.buttonUpUp.setEnabled(False)   
+        self.buttonDownDown.setEnabled(False)
+        self.startCal.setEnabled(True)
+        self.stopCal.setEnabled(False)
+        self.saveCal.setEnabled(False)
+        self.resetCal.setEnabled(False)
+        self.xValue.setReadOnly(False)
+        self.yValue.setReadOnly(False)
+        self.zValue.setReadOnly(False)
+        self.rotXValue.setReadOnly(False)
+        self.rotYValue.setReadOnly(False)
+        self.rotZValue.setReadOnly(False)
+        
     
     def performAction(self):
         if self.currentAction == "up":
@@ -372,6 +641,18 @@ class Calibrator(QWidget):
                 return
             print("Rotate Right")
             self.rotateRightFunc()  
+        elif self.currentAction == "upup":
+            if self.upUpFunc == None:
+                print("Up Up not defined")
+                return
+            print("Up Up")
+            self.upUpFunc()
+        elif self.currentAction == "downdown":
+            if self.downDownFunc == None:
+                print("Down Down not defined")
+                return
+            print("Down Down")
+            self.downDownFunc()
         else:
             print("No action")
 
