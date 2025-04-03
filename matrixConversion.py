8dc8c89f2d44bc0076bfbe795c293ed563e0c4c3
Andreas Overgaard Barbre <ao.barbre@gmail.com>
2025-04-03 08:41:02 +0200

    Rotation nearly done

--- matrixConversion.py
+++ matrixConversion.py
@@ -30,7 +30,7 @@ def axisAngleToMatrix(pose):
                      [r2[0],r2[1],r2[2],pose[1]],
                      [r3[0],r3[1],r3[2],pose[2]],
                      [0,0,0,1]])
-    return round(T,6)
+    return np.round(T,6)
 
 def matrixToRPY(pose):
     R = np.array([[pose[0][0],pose[0][1],pose[0][2]],[pose[1][0],pose[1][1],pose[1][2]],[pose[2][0],pose[2][1],pose[2][2]]])
@@ -39,5 +39,23 @@ def matrixToRPY(pose):
     p = np.arctan2(-R[2][0],np.sqrt(R[0][0]**2+R[1][0]**2))
     y = np.arctan2(R[1][0],R[0][0])
     
-    return [np.degrees(r),np.degrees(p),np.degrees(y),pose[0][3],pose[1][3],pose[2][3]]
+    return [r,p,y,pose[0][3],pose[1][3],pose[2][3]]
 
+def RPYtoRMatrix(pose):
+    r = pose[0]
+    p = pose[1]
+    y = pose[2]
+    
+    Rx = np.array([[1,0,0],
+                    [0,np.cos(r),-np.sin(r)],
+                    [0,np.sin(r),np.cos(r)]])
+    Ry = np.array([[np.cos(p),0,np.sin(p),],
+                    [0,1,0],
+                    [-np.sin(p),0,np.cos(p)]])
+    Rz = np.array([[np.cos(y),-np.sin(y),0],
+                    [np.sin(y),np.cos(y),0],
+                    [0,0,1]])
+    
+    R = Rz @ Ry @ Rx
+    
+    return R
