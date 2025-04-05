7f765dc95d11f10c3e079cfd6b90ece8c8006fae
Andreas Overgaard Barbre <ao.barbre@gmail.com>
2025-04-05 11:02:57 +0200

    f'ck rotation

--- matrixConversion.py
+++ matrixConversion.py
@@ -59,3 +59,11 @@ def RPYtoRMatrix(pose):
     R = Rz @ Ry @ Rx
     
     return R
+
+matrix = RPYtoRMatrix([0,40,20])
+# matrix = np.array([[matrix[0][0],matrix[0][1],matrix[0][2],0],
+#                     [matrix[1][0],matrix[1][1],matrix[1][2],0],
+#                     [matrix[2][0],matrix[2][1],matrix[2][2],0],
+#                     [0,0,0,1]])
+# print(matrix)
+# print(matrixToRPY(matrix))
