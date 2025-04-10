8e7c623cf472c3448d640bc7ddf5093896e9a635
Andreas Overgaard Barbre <ao.barbre@gmail.com>
2025-04-10 15:34:23 +0200

    debug remove

--- matrixConversion.py
+++ matrixConversion.py
@@ -4,7 +4,6 @@ def matrixToAxisAngle(pose):
     R = np.array([[pose[0][0],pose[0][1],pose[0][2]],[pose[1][0],pose[1][1],pose[1][2]],[pose[2][0],pose[2][1],pose[2][2]]])
     
     theta = np.arccos((R[0][0]+R[1][1]+R[2][2]-1)/2)
-    print(theta)
     
     k = (1/(2*np.sin(theta))*np.array([R[2][1]-R[1][2],R[0][2]-R[2][0],R[1][0]-R[0][1]]))
     ku = 1/np.sqrt(k[0]**2+k[1]**2+k[2]**2)*k
