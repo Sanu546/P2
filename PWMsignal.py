5a7dae9f402a5a29e63545e8787389993012c03d
Petur Hammer <phamme24@student.aau.dk>
2025-04-25 14:01:47 +0200

    pwm signal update

--- PWMsignal.py
+++ PWMsignal.py
@@ -1,18 +1,21 @@
-from gpiozero import PWMLED
+from lgpio
 import time
 
 # Vælg GPIO pin
-pwm = PWMLED(12)
+servo = 32 # GPIO pin til servo
+FREQ = 50 # Frekvens i Hz
+
+h = lgpio.gpiochip_open(0) # Åbn GPIO chip
 
 
 
 def setPwm(dutyCycle):
-    pwm.value = dutyCycle
+    lgpio.tx_pwm(h, servo, FREQ, dutyCycle)
     time.sleep(2)
 
 # dutycycle kan være mellem 0 og 1 
 while True:
-    setPwm(0.5)
+    setPwm(50)
 
 
 
