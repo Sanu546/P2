57937c9dec828ba2080ce02a32ff0da1b9460d4b
Andreas Overgaard Barbre <62778246+ismager7878@users.noreply.github.com>
2025-05-01 16:59:13 +0200

    Revert "Justering open cv"

--- PWMsignal.py
+++ PWMsignal.py
@@ -1,5 +1,5 @@
 
-#import lgpio
+import lgpio
 import time
 
 # Vælg GPIO pin
@@ -7,8 +7,8 @@ servo = 12 # GPIO pin til servo, det svarer til pin 32 på Rapsberry Pi
 FREQ = 50 # Frekvens i Hz
 
 
-#h = lgpio.gpiochip_open(0) # Åbn GPIO chip
-#lgpio.gpio_claim_output(h, servo) # Sæt GPIO pin som output
+h = lgpio.gpiochip_open(0) # Åbn GPIO chip
+lgpio.gpio_claim_output(h, servo) # Sæt GPIO pin som output
 
 
 def manualPwm():
@@ -22,12 +22,12 @@ def manualPwm():
         print("Ugyldigt input, prøv igen")
         return manualPwm()
 
-    #lgpio.tx_pwm(h, servo, FREQ, dutyCycle)
-    time.sleep(.0001)
+    lgpio.tx_pwm(h, servo, FREQ, dutyCycle)
+    time.sleep(1)
 
 def pwm(value):
-    #lgpio.tx_pwm(h, servo, FREQ, value)
-    time.sleep(.0001)
+    lgpio.tx_pwm(h, servo, FREQ, value)
+    time.sleep(1)
 
 
 
